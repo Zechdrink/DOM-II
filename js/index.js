ee283c628b7ea98b8263038c47c959aeabc664b4
Zech Drinkhall <zechdrink@aol.com>
2019-01-29 11:57:10 -0800

    added four event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,58 @@
 // Your code goes here
+
+const contentAll = document.querySelector('body');
+
+//--------- Event Listener One = Keydown --------------------
+//___________________________________________________________
+
+
+
+contentAll.addEventListener("keydown", e => {
+      if (e.key == "b") {
+      e.target.style.background = "black";
+      e.target.style.color = "silver";
+    }
+  });
+
+//--------- Event Listener Two = Keyup ---------------------
+//__________________________________________________________  
+
+  contentAll.addEventListener("keyup", e => {
+    if (e.key == "b") {
+      e.target.style.background = "";
+      e.target.style.color = "";
+    }
+  });
+
+
+ //---Event Listener's Three and Four = click and dblclick---
+
+const bttons = document.querySelectorAll('.btn');
+
+
+bttons[0].addEventListener('click',(e) => {
+    e.target.style.color = 'red';
+    e.target.style.background = 'black';
+    });
+    
+ bttons[0].addEventListener('dblclick',(e) => {
+     e.target.style.color = 'white';
+     e.target.style.background = '#17A2B8';
+    });
+    
+
+    
+//---------
+
+
+bttons[1].addEventListener('click', (e)=> {
+    e.target.style.color = 'white';
+    e.target.style.background = 'black';
+});
+
+bttons[2].addEventListener('click', (e)=> {
+    e.target.style.color = 'blue';
+    e.target.style.background = 'black';
+});
+
+
